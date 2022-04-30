fd62d3aef779837df9211b57b99ceb91087df272
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-30 20:57:08 +1000

    sign up

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react'
+
+class SignUp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            first_name: '',
+            last_name: '',
+            password: '',
+            password_confirmation: ''
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.signUp(this.state)
+    }
+    
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h1>Sign Up</h1>
+
+                <label>Email: </label>
+                <input name='email' value={this.state.email} onChange={this.handleChange} />
+
+                <label>Name: </label>
+                <input name='first_name' value={this.state.first_name} onChange={this.handleChange} />
+
+                <label>Surname: </label>
+                <input name='last_name' value={this.state.last_name} onChange={this.handleChange} />
+
+                <label>Password: </label>
+                <input name='password' type='password' value={this.state.password} onChange={this.handleChange} />
+
+                <label>Password: </label>
+                <input name='password_confirmation' type='password' value={this.state.password_confirmation} onChange={this.handleChange} />
+
+                <input type='submit' value='Register' />
+            </form>
+        );
+    }
+}
+
+export default SignUp;
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
     belongs_to :family,  :optional => true
     has_many :transactions
     has_many :categories, through: :transactions
+
+    validates :email, presence: true, uniqueness: true
+
     has_secure_password
 end
