977be5c250611c63c24b1e72afa38e4c23e5215f
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-30 17:31:46 +1000

    comment out lines related to the firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 
 import { useState, useEffect } from 'react';
 import './App.css';
-import {db} from './firebase';
+// import {db} from './firebase';
 import {collection, getDocs} from 'firebase/firestore';
 import NewExpense from './components/NewExpense';
 function App() {
   const [expenses, setExpenses] = useState([]);
-  const expensesCollectionRef = collection(db, 'Expenses');
+  // const expensesCollectionRef = collection(db, 'Expenses');
 
   useEffect(() => {
     const getExpenses = async () => {
-      const data = await getDocs(expensesCollectionRef);
-      setExpenses(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-      console.log(data.docs);
+      // const data = await getDocs(expensesCollectionRef);
+      // setExpenses(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+      // console.log(data.docs);
     };
 
     getExpenses();
