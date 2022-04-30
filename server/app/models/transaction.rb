da07c37dfcc94a811fc6071d58c5971ad4f633db
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-04-30 18:14:04 +1000

    bcrypt & pryrails gems added, amended transaction migration, seed data part done

--- server/app/models/transaction.rb
+++ server/app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
     belongs_to :user
     belongs_to :categories
-    enum type: [:expense, :income]
+    enum type_of: {:expense => 0, :income => 1}
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2022_04_30_042204) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "type"
+    t.integer "type_of", default: 0
     t.decimal "amount"
     t.text "title"
     t.text "description"
