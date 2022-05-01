5087e70339220558ec535907bc729ecd07951273
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-01 13:55:08 +1000

    add function login

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -19,6 +19,17 @@ class UsersController < ApplicationController
     render json: @user, status: :created
   end
 
+  def login
+    @user = User.find_by(email: params[:user][:email])
+
+    if @user && @user.authenticate(params[:user][:password])
+      @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+      render json: {user: @user, token: @token}
+    else
+      render json: {error: 'Invalid Credentials'}, status: :unauthorized
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
