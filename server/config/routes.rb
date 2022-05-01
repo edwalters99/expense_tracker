bb0fedfc1b2789a2fbdea76c0fa7eccd2fb35358
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-01 13:53:41 +1000

    add users#login route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:new, :create, :index, :show]
+  resources :users, :only => [:create, :index, :show]
+  post '/login', to: 'users#login'
   resources :categories
   resources :transactions, :only => [:new, :create, :index, :show]
-
 end
