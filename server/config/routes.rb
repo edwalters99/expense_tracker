5b692046988608a1063c68fea8eb518a62d006ca
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-02 13:39:43 +1000

    Rails Routes - bug fix preventing sign in

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:create, :index, :show] do
+  resources :users, :only => [:create, :index, :show]
   post '/login', to: 'users#login'
   get '/profile', to: "users#user_profile"
-  resources :categories, :only => [:index]
+  resources :categories
   resources :transactions, :only => [:new, :create, :index, :show]
-  end
 end
+
+
 
 
