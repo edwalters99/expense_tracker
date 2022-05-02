a2d7b52987bc7354576eb612185863a9f1f5c79a
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-02 13:26:46 +1000

    Seed data amended. Temporary unlocked access to User Show for debugging/testing

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:create, :index, :show]
+  resources :users, :only => [:create, :index, :show] do
   post '/login', to: 'users#login'
   get '/profile', to: "users#user_profile"
-  resources :categories
+  resources :categories, :only => [:index]
   resources :transactions, :only => [:new, :create, :index, :show]
+  end
 end
+
+
