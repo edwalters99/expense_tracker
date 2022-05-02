b727b522fae088c18d7472c33a0c04f3edce07bd
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-03 09:03:50 +1000

    remove impot axios

--- client/src/components/Transaction/NewTransaction.js
+++ client/src/components/Transaction/NewTransaction.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import TransactionForm from './TransactionForm';
 import './NewTransaction.css';
-import axios from 'axios';
 
 const NewTransaction = (props) => {
     const [isEditing, setIsEditing] = useState(false);
