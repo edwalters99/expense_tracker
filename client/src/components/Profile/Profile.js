65be58a21b69afed39699ccf244915f9add0aac5
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-05-03 18:56:13 +1000

    Change password, server receiving request but not actioning in db

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ProfileView from './ProfileView';
 import createRequest from '../../request';
 import { Button } from 'react-bootstrap';
+import './Profile.css'
 
 const Profile = (props) => {
     const [state, setState] = useState( {
@@ -22,6 +23,10 @@ const Profile = (props) => {
             <div>
                 <ProfileView view={ state } />
                 <Button className="offset-md-4" variant="secondary" href="/profile/edit">Edit Profile</Button> 
+                <span className='changepassword'>
+                    <Button className='btn-changepassword' variant="secondary" href="/profile/changepassword">Change Password</Button> 
+                </span>
+                
             </div>
     )
 }
