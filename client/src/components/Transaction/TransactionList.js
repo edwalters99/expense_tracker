180bdec836bc950ffafd446c9c8254e5f74c4fab
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-03 20:52:17 +1000

    delete transaction

--- client/src/components/Transaction/TransactionList.js
+++ client/src/components/Transaction/TransactionList.js
@@ -4,26 +4,28 @@ import './TransactionList.css'
 
 
 const TransactionList = (props) => {
+  // console.log(props)
   if (props.items.length === 0) {
     return <h2>Found no transaction.</h2>;
   } else {
     return (
       <ul className='transaction-list'>
-        {/* {console.log(props.items, 'else')} */}
         {props.items.map((transaction) => (
-          <TransactionItem
-            key={transaction.id}
-            title={transaction.title}
-            type={transaction.type}
-            description={transaction.description}
-            amount={transaction.amount}
-            date={transaction.date}
-          />
+          <div key={transaction.id}>
+            <TransactionItem
+              id={transaction.id}
+              title={transaction.title}
+              type={transaction.type_of}
+              description={transaction.description}
+              amount={transaction.amount}
+              date={transaction.date}
+              onDeleteTransaction={props.onDeleteTransaction}
+            />
+          </div>
         ))}
       </ul>
     );
   }
-
 
 };
 
