62a800d84caf6523419bc33a3492d54f56f6edc5
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-04 17:00:21 +1000

    remove puts (debug)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,6 @@ class ApplicationController < ActionController::Base
         end
 
         if user_id
-            puts "NAME" 
-            puts user_id
             @user = User.find(user_id)
         else
             nil
