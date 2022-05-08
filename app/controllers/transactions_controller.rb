493c7cc6f6a7a4c2bd587336d2e15ca56be86d64
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-08 17:32:13 +1000

    refactor code transaction edit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,23 +5,19 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @transaction = Transaction.create(transaction_params)
-    @user.transactions << @transaction  
-    render json: @transaction, status: :created
+    transaction = Transaction.create(transaction_params)
+    @user.transactions << transaction  
+    render json: transaction, status: :created
   end
 
   def update
-    @transaction = Transaction.find params[:id]
-      if @transaction.update(transaction_params)
-        render json: @transaction, status: :created 
+    transactions = @user.transactions
+    transaction = transactions.find(params[:id])
+      if transaction.update(transaction_params)
+        render json: transaction, status: :created 
       else
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+        format.json { render json: transaction.errors, status: :unprocessable_entity }
       end
-  end
-
-  def show 
-    @transaction = Transaction.find params[:id]
-    render json: @transaction
   end
 
   def destroy
@@ -33,7 +29,7 @@ class TransactionsController < ApplicationController
 
   private
   def transaction_params
-      params.require(:transaction).permit(:id,:type_of, :amount, :title, :description, :date, :receipt, :category_id, :user_id)
+      params.require(:transaction).permit(:type_of, :amount, :title, :description, :date, :receipt, :category_id, :user_id)
   end
 end
 
